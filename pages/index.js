535c0af4aefb72aa8b00ad707bf98b924485b348
Volodymyr Mykolaichyk <vmykolaichyk@gmail.com>
2023-07-10 17:52:12 +0300

    feat: add some text to main page

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,36 @@ export default function Home() {
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
         </p>
+        <p>
+          lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod
+          voluptatibus, quae, voluptatem, quos voluptas quia quibusdam
+          accusantium quas voluptatum dolorum. Quisquam quod voluptatibus,
+          quae, voluptatem, quos voluptas quia quibusdam accusantium quas
+          voluptatum dolorum. Quisquam quod voluptatibus, quae, voluptatem,
+        </p>
+        
+        <p>
+          lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod
+          </p>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-12 col-md-6">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and make
+                      up the bulk of the card's content.
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+              </div>
+              </div>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
